7480389efcbe87a22845243ca486cc6cffce1a3c
Harshith Bezawada <harsh20y@gmail.com>
2024-01-31 06:57:05 +0530

    Stack Implementation By LinkedLists

---
+++ LinkedList/QueueByLinkedList.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max 10
+struct Node{
+    int data;
+    struct Node *link;
+};
+typedef struct Node *NODE;
+NODE front,rear;
+NODE getNode(int data){
+    NODE newNode = (NODE)malloc(sizeof(struct Node));
+    newNode->data=data;
+    newNode->link=NULL;
+    return newNode;
+}
+void enqueue(int val){
+    NODE newNode = getNode(val);
+    if(front==NULL && rear==NULL){
+        front=newNode;
+        rear=newNode;
+        return;
+    }
+    rear->link=newNode;
+    rear=newNode;
+}
+void dequeue(){
+    if(front==NULL && rear==NULL){
+        printf("Empty queue");
+        return;
+    }
+    if(front==rear){
+        printf("%d is popped",front->data);
+        free(front);
+        free(rear);
+        front=NULL;
+        rear=NULL;
+        return;
+    }
+    NODE temp=front;
+    front=front->link;
+    printf("%d is removed",temp->data);
+    free(temp);
+}
+void display(){
+    if(front==NULL && rear==NULL){
+        printf("Empty queue");
+        return;
+    }
+    NODE temp1=front;
+    printf("Queue :\n");
+    while(temp1!=rear){
+        printf("%d\n",temp1->data);
+        temp1=temp1->link;
+    }
+    printf("%d",temp1->data);
+    free(temp1);
+}
+void main(){
+    NODE front=NULL,rear=NULL;
+    int ch,ele;
+    while(1){
+        printf("Enter your choice \n1.Enqueue\n2.Dequeue\n3.Display");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1 : printf("Enter the element you want to add");
+                     scanf("%d",&ele);
+                     enqueue(ele);
+                     break;
+            case 2 : dequeue();
+                     break;
+            default: display();
+                     exit(0);
+        }
+    }
+}
+
